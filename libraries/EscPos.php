95f8189000c68c5dd5a5e08ecaabec46ca5112f2
Mr. D <desta.08b@gmail.com>
2017-05-10 10:35:36 +0700

    Just remove
    remove some unused code

--- libraries/EscPos.php
+++ libraries/EscPos.php
@@ -1,23 +1,11 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-  
-  // using by vendor
-  // class Guzzle {
-  //   public function Guzzle() {
-  //     // require_once('vendor/autoload.php');
-  //     require 'vendor/autoload.php';
-  //   }
-  // }
-  
-
   class Escpos
   {
   	public function __construct()
   	{
-  		// require_once APPPATH . 'third_party/guzzle/autoloader.php';
       require_once APPPATH . 'third_party/Mike42/autoloader.php';
-      // require_once APPPATH . 'third_party/guzzle6/autoloader.php';
   	}
   }
   
